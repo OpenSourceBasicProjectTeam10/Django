460e8289fe45b7001b3c8f5b1a17e0fc59a61a35
kang taekwang <blognolnolja@gmail.com>
2024-05-19 11:53:16 +0900

    db_setting_commit

--- CBNUPORTAL/CBNUPORTAL/settings.py
+++ CBNUPORTAL/CBNUPORTAL/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'CBNUPORTAL.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', # mysqlclient librarly 설치
+        'NAME': 'cbnu_portal',
+        'USER': 'root',
+        'PASSWORD': '1234', # mariaDB 설치 시 입력한 root 비밀번호 입력
+        'HOST': 'localhost',
+        'PORT': ''
     }
 }
 
